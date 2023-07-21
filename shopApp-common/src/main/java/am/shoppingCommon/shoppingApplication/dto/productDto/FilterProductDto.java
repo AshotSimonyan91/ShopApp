d05a69b71cf40af151ec45b5412333e36ea66393
dell <hrant3307@gmail.com>
2023-07-21 23:18:12 +0400

    add tests and fix filter

--- shopApp-common/src/main/java/am/shoppingCommon/shoppingApplication/dto/productDto/FilterProductDto.java
+++ shopApp-common/src/main/java/am/shoppingCommon/shoppingApplication/dto/productDto/FilterProductDto.java
@@ -1,25 +1,20 @@
 package am.shoppingCommon.shoppingApplication.dto.productDto;
 
-import am.shoppingCommon.shoppingApplication.dto.categoryDto.CategoryDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class FilterProductDto {
-    private String name;
     private String brand;
     private String productCode;
     private double minPrice;
     private double maxPrice;
-    private List<CategoryDto> categories;
-    private String serialNumber;
+//    private List<Integer> categoryIds;
     private String sortBy;
     private String sortDirection;
 }
