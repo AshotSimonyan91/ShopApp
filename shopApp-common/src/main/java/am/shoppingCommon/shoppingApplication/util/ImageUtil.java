b7892988fb688f7da36aeb99c911ce611898dcec
dell <hrant3307@gmail.com>
2023-07-26 22:07:40 +0400

    image resizing

--- shopApp-common/src/main/java/am/shoppingCommon/shoppingApplication/util/ImageUtil.java
+++ shopApp-common/src/main/java/am/shoppingCommon/shoppingApplication/util/ImageUtil.java
@@ -37,24 +37,20 @@ public class ImageUtil {
     private static void resizeAndSaveImage(MultipartFile originalImage, int targetWidth, int targetHeight, File outputFile) throws IOException {
         BufferedImage img = ImageIO.read(originalImage.getInputStream());
 
-        // Create a resized image with the specified dimensions
         BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, img.getType());
         Graphics2D g2d = resizedImage.createGraphics();
 
-        // Use bicubic interpolation for better image quality
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
         g2d.drawImage(img, 0, 0, targetWidth, targetHeight, null);
         g2d.dispose();
 
-        // Determine the file format
         String formatName = getFileFormat(originalImage.getOriginalFilename());
 
-        // Save the resized image to the file
         ImageIO.write(resizedImage, formatName, outputFile);
     }
 
     private static String getFileFormat(String fileName) {
-        String formatName = "jpg"; // Default to jpg format
+        String formatName = "jpg";
 
         int dotIndex = fileName.lastIndexOf('.');
         if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
